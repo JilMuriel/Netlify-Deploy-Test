85eb008f2bca08c98afd7637afb099627c8eb8f5
Jil Muriel <djibrilmuriel@gmail.com>
2021-08-17 13:31:25 +0800

    update text

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">
-    <h1 className="text-2xl">Hello world</h1>
-  </div>;
+  return (
+    <div className="App">
+      <h1 className="text-2xl">Hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere velit
+        fuga quibusdam adipisci. Nulla corporis sed, repellendus laboriosam aut,
+        minima voluptates qui consequatur quas consequuntur tenetur quisquam
+        ipsa, fugiat blanditiis?
+      </p>
+    </div>
+  );
 }
 
 export default App;
